15d743668f6036d923b7c34dd7c8dea34eef8262
weishen1223 <31945615+weishen1223@users.noreply.github.com>
2017-11-12 20:55:58 +0800

    Update admin.js

--- admin.js
+++ admin.js
@@ -43,15 +43,6 @@ window.fbAsyncInit = function() {
   js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11&appId=234394523755744';
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
-// var pendingKey = [];
-// var approveKey = [];
-//---------------------------------UUID v4---------------------------------------
-// function uuidv4() {
-//   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-//     var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-//     return v.toString(16);
-//   });
-// }
 
 //---------------------------------handleSignIn------------------------------------
 
